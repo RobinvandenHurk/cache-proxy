9faacfb3a49179edcf2e45bf3da87d06fc658b28
Robin van den Hurk <robinvandenhurk@live.nl>
2022-09-08 16:49:40 +0200

    Added port  argument

--- proxy.py
+++ proxy.py
@@ -11,12 +11,13 @@ from lib.server import Server
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Your local caching proxy')
     parser.add_argument('-s', dest='session_file', type=str, help='Use session file')
+    parser.add_argument('-p', dest='port', type=int, help='Port (Default: 5000)', default=5000)
 
     args = parser.parse_args()
 
     colorama.init()
     cache = Cache(args.session_file)
-    process = Process(target=Server(5000, cache).start)
+    process = Process(target=Server(args.port, cache).start)
     process.start()
 
     try:
